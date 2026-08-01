51c6c65c9fdd981213e8ea193d5a9e4c965f2263
MMAAXXIII <160916593+MMAAXXIII@users.noreply.github.com>
2026-08-01 22:45:06 +0200

    feat: add traceable scientific observations

--- src/ecobiome/core/observation/__init__.py
+++ src/ecobiome/core/observation/__init__.py
@@ -1,7 +1,15 @@
 """Scientific observation primitives."""
 
 from ecobiome.core.observation.measurement import ScientificMeasurement
+from ecobiome.core.observation.observation import (
+    AcquisitionMethod,
+    Observation,
+    ObservationValue,
+)
 
 __all__ = [
+    "AcquisitionMethod",
+    "Observation",
+    "ObservationValue",
     "ScientificMeasurement",
 ]
