3b4a34f3a6905dbbb5043e676ecf54d41938b87a
MMAAXXIII <160916593+MMAAXXIII@users.noreply.github.com>
2026-08-01 23:26:27 +0200

    feat: model observation data quality

--- src/ecobiome/core/observation/__init__.py
+++ src/ecobiome/core/observation/__init__.py
@@ -10,12 +10,20 @@ from ecobiome.core.observation.observation_store import (
     InMemoryObservationStore,
     ObservationStore,
 )
+from ecobiome.core.observation.quality import (
+    DataQuality,
+    DiagnosticCode,
+    QualityAssessment,
+)
 
 __all__ = [
     "AcquisitionMethod",
+    "DataQuality",
+    "DiagnosticCode",
     "InMemoryObservationStore",
     "Observation",
     "ObservationStore",
     "ObservationValue",
+    "QualityAssessment",
     "ScientificMeasurement",
 ]
